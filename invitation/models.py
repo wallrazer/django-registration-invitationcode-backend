ef157fd92a3d630de7672e36bc1da4422ed4bfb8
Steve Ivy <steve@wallrazer.com>
2012-07-13 21:43:24 -0400

    Add send activation email so that it sends html mail. this is so that the html backend doesn't override the invitation code.

--- invitation/models.py
+++ invitation/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
-import random
-import string
+from django.conf import settings
 
 
 class InvitationCode(models.Model):
@@ -15,3 +14,22 @@ class InvitationCode(models.Model):
     used_date = models.DateTimeField(blank=True, null=True, auto_now_add=True,
         verbose_name=_(u"Used on"))
 
+    def send_activation_email(self, site):
+        """Send the activation mail"""
+        from django.core.mail import EmailMultiAlternatives
+        from django.template.loader import render_to_string
+
+        ctx_dict = {'activation_key': self.activation_key,
+                    'expiration_days': settings.ACCOUNT_ACTIVATION_DAYS,
+                    'site': site}
+        subject = render_to_string('registration/activation_email_subject.txt',
+                                   ctx_dict)
+        # Email subject *must not* contain newlines
+        subject = ''.join(subject.splitlines())
+
+        message_text = render_to_string('registration/activation_email.txt', ctx_dict)
+        message_html = render_to_string('registration/activation_email.html', ctx_dict)
+
+        msg = EmailMultiAlternatives(subject, message_text, settings.DEFAULT_FROM_EMAIL, [self.user.email])
+        msg.attach_alternative(message_html, "text/html")
+        msg.send()
